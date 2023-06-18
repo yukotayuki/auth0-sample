cc89a9a2f7a34e9bc127cdcfe65a609fe7568759
joo <yukota.yuki@hotmail.com>
2023-06-19 00:39:34 +0900

    feat: add api server created by express

---
+++ web-server.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const app = express();
+const { auth, requiredScopes } = require("express-oauth2-jwt-bearer");
+const authConfig = require("./.auth_config.json");
+
+// Authorization middleware. When used, the Access Token must
+// exist and be verified against the Auth0 JSON Web Key Set.
+const checkJwt = auth({
+  audience: authConfig.audience,
+  issuerBaseURL: `https://${authConfig.domain}`,
+});
+
+// This route doesn't need authentication
+app.get("/api/public", function (req, res) {
+  res.json({
+    message:
+      "Hello from a public endpoint! You don't need to be authenticated to see this.",
+  });
+});
+
+// This route needs authentication
+app.get("/api/private", checkJwt, function (req, res) {
+  res.json({
+    message:
+      "Hello from a private endpoint! You need to be authenticated to see this.",
+  });
+});
+
+const checkScopes = requiredScopes("read:messages");
+
+app.get("/api/private-scoped", checkJwt, checkScopes, function (req, res) {
+  res.json({
+    message:
+      "Hello from a private endpoint! You need to be authenticated and have a scope of read:messages to see this.",
+  });
+});
+
+app.listen(3000, function () {
+  console.log("Listening on http://localhost:3000");
+});
